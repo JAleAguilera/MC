ace25975fd009d5ca9ef29b2ab448c8b77af029a
Jenifer Alejandra Aguilera Huertas <ja.aguilera2276@uniandes.edu.co>
2015-06-10 23:45:16 -0500

    Punto 2-A

---
+++ Tareas/HW2/horton1-1.c
@@ -0,0 +1,10 @@
+/* Porgrama en C que muestra el nombre y la dirección */
+
+# include <stdio.h>
+
+int main (void)
+{
+	printf("Meine name ist: \nJenifer Alejandra Aguilera Huertas") 
+	printf("Meine adress: \nKra 86 # 77 -35");
+	return 0;
+}
