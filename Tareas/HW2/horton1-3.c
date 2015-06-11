f457ff79ea015cc16793c63bf63b9f96d4f7a4f3
Jenifer Alejandra Aguilera Huertas <ja.aguilera2276@uniandes.edu.co>
2015-06-10 23:59:50 -0500

    Punto 2

---
+++ Tareas/HW2/horton1-3.c
@@ -0,0 +1,9 @@
+/* Programa C que muestra la frase: "It's freezing in here. He said coldly"*/
+
+# include <stdio.h>
+
+int main (void)
+{
+	printf("\It\'s freezing in here,\" he said coldly.\n"); 
+	return 0;
+}
